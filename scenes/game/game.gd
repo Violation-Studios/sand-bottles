c31a791c9fcd19fa68694dc4e6f326c4e3e4ef0b
nikolaoslibero <65852657+nikolaoslibero@users.noreply.github.com>
2022-11-10 13:30:08 -0500

    fix board position bug
    No idea what the problem was, but through refactoring it disappeared. Was creating all bottles and then getting a reference to them from an array, now sets position after each bottle is created instead of in a batch from an array. *shrug*

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -4,31 +4,40 @@ var Bottle = preload("res://scenes/bottle/bottle.tscn")
 
 var bottles = []
 var selected_bottles = []
-var board_size = Vector2(4,4)
 
 
 func _ready():
-	start_game(16,1,1,board_size)
+	setup_board(4,4)
 
 
-func _process(delta):
+func _process(_delta):
 	for bottle in selected_bottles:
+		if bottle.rotation >= 6.28319:
+			bottle.rotation = 0.0
 		bottle.rotation += 0.01
 
 
-func start_game(bottle_quantity:int, color_quantity:int, part_quantity:int, grid_size:Vector2):
-	for bottle in bottle_quantity:
-		var new_bottle = Bottle.instance()
-		add_child(new_bottle)
-		new_bottle.connect("selected", self, "_on_Bottle_selected")
-		bottles.push_back(new_bottle)
-		
-	for row in range(1, grid_size.y + 1):
-		for column in range(1, grid_size.x + 1):
-			print(Vector2(row,column))
-			bottles[column * row - 1].position.x = 192 * column
-			bottles[column * row - 1].position.y = 384 * row
-			print(bottles[column * row - 1].position)
+func create_bottle():
+	var new_bottle = Bottle.instance()
+	add_child(new_bottle)
+	new_bottle.connect("selected", self, "_on_Bottle_selected")
+	bottles.push_back(new_bottle)
+	return new_bottle
+
+
+func setup_board(rows: int, columns: int):
+	var column_width: int = 192
+	var row_height: int = 384
+	
+	for row in range(1, rows + 1):
+		for column in range(1, columns + 1):
+			var bottle = create_bottle()
+			bottle.position.x = column_width * column
+			bottle.position.y = row_height * row
+			
+			print(Vector2(row,column), " - ", bottle.position)
+
 
 func _on_Bottle_selected(bottle):
 		selected_bottles.push_back(bottle)
+		print(bottle.position)
