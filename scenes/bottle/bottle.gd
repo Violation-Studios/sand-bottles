8279cb12a51ed7c1a80ded16af8147d929fade99
nikolaoslibero <65852657+nikolaoslibero@users.noreply.github.com>
2022-11-11 02:03:54 -0500

    create section scene
    Sections are the colored chunks of "sand" in the cups. Multiple sections can technically be added currently, but their size and position is not yet set for sections after one. Color not yet implemented.

--- scenes/bottle/bottle.gd
+++ scenes/bottle/bottle.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
+var sections = []
+
 signal selected(bottle)
-
-onready var BOTTLE_SIZE: Vector2 = $Area2D/CollisionPolygon2D.shape.extents * 2
-
-func _ready():
-	pass
 
 
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var Bottle = preload("res://scenes/bottle/bottle.tscn")
+var Section = preload("res://scenes/bottle/section/section.tscn")
 
 var bottles = []
 var selected_bottles = []
@@ -12,26 +13,32 @@ func _ready():
 
 func _process(_delta):
 	for bottle in selected_bottles:
-		if bottle.rotation >= 6.28319:
+		if bottle.rotation >= TAU:
 			bottle.rotation = 0.0
 		bottle.rotation += 0.01
 
 
-func create_bottle():
+func create_bottle(section_quantity: int, _color_quantity: int):
 	var new_bottle = Bottle.instance()
 	add_child(new_bottle)
 	new_bottle.connect("selected", self, "_on_Bottle_selected")
 	bottles.push_back(new_bottle)
+	
+	for section in section_quantity:
+		var new_section = Section.instance()
+		new_bottle.sections.push_back(new_section)
+		new_bottle.add_child(new_section)
+	
 	return new_bottle
 
 
 func setup_board(rows: int, columns: int):
-	var column_width: int = get_viewport_rect().size.x / columns
-	var row_height: int = get_viewport_rect().size.y / rows
+	var column_width = get_viewport_rect().size.x / columns
+	var row_height = get_viewport_rect().size.y / rows
 	
 	for row in range(1, rows + 1):
 		for column in range(1, columns + 1):
-			var bottle = create_bottle()
+			var bottle = create_bottle(1,1)
 			bottle.position.x = column_width * column - (column_width / 2)
 			bottle.position.y = row_height * row - (row_height / 2)
 			
