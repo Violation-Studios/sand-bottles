e5c7ce754f7eeee5197b840d1a9d807ed9648e6e
nikolaoslibero <65852657+nikolaoslibero@users.noreply.github.com>
2022-11-16 21:47:16 -0500

    Squashed commit of the following:
    commit 1ad6e6e47a683f1264965bd3f23ddc986dd69ea7
    Author: nikolaoslibero <65852657+nikolaoslibero@users.noreply.github.com>
    Date:   Wed Nov 16 21:45:17 2022 -0500
    
        fix and make winnable
    
    commit 5a5411b705e145a527d63c1a5106035ddab1fb56
    Author: nikolaoslibero <65852657+nikolaoslibero@users.noreply.github.com>
    Date:   Mon Nov 14 05:02:22 2022 -0500
    
        Update game.gd
    
        refactoring...
    
        getting "Invalid get index '0' (on base: 'Array')." in game.gd on line 37 at function:  fill_bottles_from. section_list is somehow empty. as is grid_list and color_list.

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,84 +1,133 @@
-extends Node2D
+class_name Game extends Node2D
 
-var Bottle = preload("res://scenes/bottle/bottle.tscn")
-var Section = preload("res://scenes/bottle/section/section.tscn")
+const Bottle = preload("res://scenes/bottle/bottle.tscn")
+const Section = preload("res://scenes/bottle/section/section.tscn")
 
 var colors = [
 	Color("#0d2b45"), Color("#203c56"), Color("#544e68"), Color("#8d697a"),
 	Color("#d08159"), Color("#ffaa5e"), Color("#ffd4a3"), Color("#ffecd6"),
 ]
-var bottles = []
 var selected_bottle: Bottle = null
-var section_quantity: int = 5
+const bottle_quantity: int = 9
+const bottle_fill_amount: int = 8
+const bottle_capacity: int = 9
+const color_quantity:int = 8
+const board_columns: int = 3
 
 
 func _ready():
-	setup_board(4,3)
-	for section in bottles[0].sections:
-		section.queue_free()
-	bottles[0].sections.clear()
+	var grid_list = create_grid_list(bottle_quantity, board_columns)
+	var bottle_list = create_bottle_list_from(grid_list)
+	var color_list = create_color_list(bottle_list.size(), bottle_fill_amount, color_quantity)
+	var section_list = create_section_list(color_list)
+	var _filled_list = fill_bottles_from(section_list, bottle_list, bottle_fill_amount)
 
 
-func _process(_delta):
-	pass
-
-
-func create_bottle():
-	var new_bottle = Bottle.instance()
+func fill_bottles_from(section_list: Array, bottle_list: Array, fill_amount: int):
+	var filled_list = []
 	
-	fill_bottle(new_bottle, 5)
-	
-	add_child(new_bottle)
-	new_bottle.connect("selected", self, "_on_Bottle_selected")
-	bottles.push_back(new_bottle)
-	
-	return new_bottle
-
-
-func fill_bottle(bottle: Bottle, color_quantity: int):
-	for section in section_quantity:
+	var fill_index = 1
+	var bottle_index = 0
+	var section_index = 0
+	while section_index < section_list.size():
+		if fill_index > fill_amount:
+			fill_index = 1
+			bottle_index += 1
 		
+		bottle_list[bottle_index].add_child(section_list[section_index])
+		bottle_list[bottle_index].sections.push_back(section_list[section_index])
+		
+		section_list[section_index].scale.y /= bottle_capacity
+		section_list[section_index].position.y = position_section_at(fill_index - 1)
+		
+		fill_index += 1
+		if fill_index > fill_amount:
+			filled_list.push_back(bottle_list[bottle_index])
+			
+		section_index += 1
+	
+	return filled_list
+
+
+func create_section_list(color_list: Array):
+	var section_list = []
+	
+	for color in color_list:
 		var new_section = Section.instance()
-		
-		new_section.scale.y /= section_quantity
-		new_section.color = color_section(color_quantity)
-		new_section.position.y = position_section(section)
-		
-		bottle.sections.push_back(new_section)
-		bottle.add_child(new_section)
+		new_section.color = color
+		section_list.push_back(new_section)
+	
+	return section_list
 
 
-func color_section(color_quantity: int):
+func create_color_list(_bottle_quantity: int, fill_amount: int, total_colors: int):
+	var color_list = []
+	var colors_needed: int = _bottle_quantity * fill_amount
 	var rng = RandomNumberGenerator.new()
-	rng.randomize()
+	var color_slice = colors.slice(0,total_colors - 1,1)
 	
-	var picked_color = rng.randi_range(0, color_quantity - 1)
-	return colors[picked_color]
-
-
-func position_section(position):
-	return (-120.0 / section_quantity) * (1 + (position * 2)) +120
-
-
-func setup_board(rows: int, columns: int):
-	var column_width = get_viewport_rect().size.x / columns
-	var row_height = get_viewport_rect().size.y / rows
+	for i in color_quantity + 1:
+		color_list += color_slice
+	color_list.shuffle()
 	
-	for row in range(1, rows + 1):
-		for column in range(1, columns + 1):
-			var bottle = create_bottle()
-			bottle.position.x = column_width * column - (column_width / 2)
-			bottle.position.y = row_height * row - (row_height / 2)
+	for color in colors_needed:
+		rng.randomize()
+		var picked_color = colors[rng.randi_range(0, total_colors - 1)]
+		#color_list.push_back(picked_color)
+		
+	return color_list
+
+
+func position_section_at(location):
+	return (-120.0 / bottle_capacity) * (1 + (location * 2)) +120
+
+
+func create_bottle_list_from(list: Array):
+	var bottle_list = []
+	var space_width = get_viewport_rect().size.x / float(board_columns)
+	var space_height = get_viewport_rect().size.y / ceil(bottle_quantity / float(board_columns))
+	var space_size = Vector2(space_width, space_height)
+	var bottle_scale_multiplier = 1.0 / sqrt(sqrt(bottle_quantity) * board_columns)
+	
+	for location in list:
+		var new_bottle = Bottle.instance()
+	
+		add_child(new_bottle)
+		new_bottle.scale *= bottle_scale_multiplier
+		new_bottle.position = space_size * location - (space_size / 2)
+		
+		new_bottle.connect("selected", self, "_on_Bottle_selected")
+		bottle_list.push_back(new_bottle)
+	
+	return bottle_list
+
+
+func create_grid_list(number_of_spaces, number_of_columns):
+	var grid_list = []
+	
+	var row_index = 1
+	var column_index = 1
+	var spaces_to_generate = number_of_spaces
+	while spaces_to_generate > 0:
+		if column_index > number_of_columns:
+			column_index = 1
+			row_index += 1
+		
+		grid_list.push_back(Vector2(column_index, row_index))
+		spaces_to_generate -= 1
+		column_index += 1
+	
+	return grid_list
 
 
 func try_move_section(from: Bottle, to: Bottle):
-	if from.sections.size() > 0 and to.sections.size() < section_quantity:
+	if from.sections.size() > 0 and to.sections.size() < bottle_capacity:
 		from.remove_child(from.sections.back())
 		to.add_child(from.sections.back())
 		
 		to.sections.push_back(from.sections.pop_back())
 		
-		to.sections[-1].position.y = position_section(to.sections.size() - 1)
+		to.sections[-1].position.y = position_section_at(to.sections.size() - 1)
 
 
 func _on_Bottle_selected(bottle):
