8ed8ca627447ec6e0a0fb218dd6eee96d0b5e09a
nikolaoslibero <65852657+nikolaoslibero@users.noreply.github.com>
2022-11-28 00:53:13 -0500

    Update bottle.gd
    add function to check if bottle is mixed

--- scenes/bottle/bottle.gd
+++ scenes/bottle/bottle.gd
@@ -5,6 +5,17 @@ var sections = []
 signal selected(bottle)
 
 
+func is_bottle_unmixed():
+	var bottle_is_unmixed: bool = true
+	
+	for section in sections:
+		if section.color != sections.front().color:
+			bottle_is_unmixed = false
+			break
+	
+	return bottle_is_unmixed
+
+
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
 	if event is InputEventScreenTouch:
 		if event.is_pressed():
